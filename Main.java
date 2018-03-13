846435d7f394a3681e0bf121e45a0af4c1ff2d48
tidi03 <37335792+tidi03@users.noreply.github.com>
2018-03-13 13:59:07 +0100

    änderung tim ist cool

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package ch.hauksson.elias.problems.primeNumbers;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Tim ist cool");
 	}
 
 }
